3d3b381b91a0a439fdaee6302e75095157cc9c02
Frederico Amaral <fredericoamaral@Fredericos-MacBook-Pro.local>
2020-11-01 18:08:00 -0300

    GET ROUTE

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const server = express();
 
-server.listen(8000,function(){
+server.listen(8000, function () {
     console.log("server is running")
+})
+
+const mockUserData = [{
+        name: 'Mark'
+    },
+    {
+        name: 'Jill'
+    }
+]
+
+server.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
     })
+})
