f27fab5313da501468c1192b3b894fec1b739d15
Frederico Amaral <fredericoamaral@Fredericos-MacBook-Pro.local>
2020-11-01 18:26:39 -0300

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
+
+server.use(bodyParser.json());
 
 server.listen(8000, function () {
     console.log("server is running")
@@ -28,4 +31,27 @@ server.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+server.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="fred";
+    const mockPassword="12345";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              messagee: 'password and username do not match',
+              pass: password,
+              user: username
+         })
+    }
 })
